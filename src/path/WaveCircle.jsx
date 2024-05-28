308676cd01eae7140d0a8ba9667bd44e9af52733
Koppeks <koppeks.contactos@gmail.com>
2024-05-28 01:23:05 -0300

    stackoverflow changes

--- src/path/WaveCircle.jsx
+++ src/path/WaveCircle.jsx
@@ -15,31 +15,15 @@ function WaveCircles({ circles }) {
   const [indexOfCircles, setIndexOfCircles] = useState(0);
   const progress = useMotionValue(0);
   const getIndex = circles.length > 0 ? circles.map((_, i) => i) : [];
-  // const circlesValues = useTransform(progress, getIndex, circles,  
-  //     (a, b) => ({
-  //     cx: interpolate(a.cx, b.cx, progress),
-  //     cy: interpolate(a.cy, b.cy, progress),
-  //     r: interpolate(a.r, b.r, progress),
-  //     opacity: interpolate(a.opacity, b.opacity, progress),
-  //   })
-  // );
-  // const pathValue = useTransform(progress, pathIndex, paths, {
-  //   mixer: (a, b) => interpolate(a,b)
-  // })
-  const cxValue = useTransform(progress, getIndex, circles,  
-      {mixer: (curr, prev) => {console.log(curr.cx)}}
+  const circlesValues = useTransform(progress, getIndex, circles,  
+      (a, b) => ({
+      cx: interpolate(a.cx, b.cx, progress),
+      cy: interpolate(a.cy, b.cy, progress),
+      r: interpolate(a.r, b.r, progress),
+      opacity: interpolate(a.opacity, b.opacity, progress),
+    })
   );
-  // const cyValue = useTransform(progress, getIndex, circles,  
-  //     {mixer: (a, b) => interpolate(a.cy, b.cy)}
-  // );
-  // const rValue = useTransform(progress, getIndex, circles,  
-  //     {mixer: (a, b) => interpolate(a.r, b.r)}
-  // );
-  // const opacityValue = useTransform(progress, getIndex, circles,  
-  //     {mixer: (a, b) => interpolate(a.opacity, b.opacity)}
-  // );
 
-  
   useEffect(() => {
       animate(progress, indexOfCircles, {
         duration: 0.5,
@@ -59,10 +43,10 @@ function WaveCircles({ circles }) {
 
   return (
     <motion.circle
-      cx={cxValue}
-      // cy={cyValue}
-      // r={rValue}
-      // opacity={opacityValue}
+      cx={circlesValues.current.cx}
+      cy={circlesValues.current.cy}
+      r={circlesValues.current.r}
+      opacity={circlesValues.current.opacity}
       stroke="#6b97ff"
       strokeWidth={16}
     />
